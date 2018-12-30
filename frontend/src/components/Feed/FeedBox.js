5e18ad27b0de778bf5d4a125d91246a2d8ece50b
Quek Ruo Ling <>
2018-12-30 22:31:37 +0800

    Remove rich text editor from Feed page

--- frontend/src/components/Feed/FeedBox.js
+++ frontend/src/components/Feed/FeedBox.js
@@ -1,16 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Editor from "./Editor";
 
-import { Button } from "../Button";
-
-const TextBox = styled.div`
-    background-color: #fff;
-    border: 1px solid #e0e0e0;
-    padding: 10px;
-
-    ${props => (props.focus ? "border: 1px solid #f07241;" : null)};
-`;
 
 const StyledFeedBox = styled.div`
     display: flex;
@@ -20,49 +10,20 @@ const StyledFeedBox = styled.div`
 
 const PostButton = Button.extend`
     align-self: flex-end;
-    margin-top: 10px;
 `;
 
-class RichEditor extends React.Component {
-    state = {
-        editorState: {}
-    };
 
-    onChange = editorState => {
-        this.setState({ editorState });
-    };
-
-    render() {
-        let { onFocus, onBlur, focus } = this.props;
-        return (
-            <TextBox focus={focus}>
-                <Editor
-                    editorState={this.state.editorState}
-                    onChange={this.onChange}
-                    onFocus={onFocus}
-                    onBlur={onBlur}
-                    placeholder="What's on your mind?"
-                />
-            </TextBox>
-        );
     }
-}
 
 class FeedBox extends React.Component {
     state = {
-        focus: false
     };
 
-    onFocus = () => {
-        this.setState({ focus: true });
     };
 
-    onBlur = () => {
-        this.setState({ focus: false });
     };
 
     render() {
-        let { handleOnPostChange, handleOnPostSubmit } = this.props;
         return (
             <StyledFeedBox>
                 <RichEditor
@@ -71,9 +32,6 @@ class FeedBox extends React.Component {
                     focus={this.props.focus}
                     onChange={handleOnPostChange}
                 />
-                {this.state.focus && (
-                    <PostButton onClick={handleOnPostSubmit}>Post</PostButton>
-                )}
             </StyledFeedBox>
         );
     }
--- frontend/src/components/Feed/components/Span.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Span = styled.span`
-    padding: 0;
-    line-height: 1rem;
-    min-height: 1rem;
-`;
-
-export default Span;
