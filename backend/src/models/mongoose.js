6c02c86a5299933725ea9cb6da4bdfdc01ad5828
Quek Ruo Ling <>
2018-12-31 15:15:59 +0800

    [Backend] Update mongo db libraries and disable buffering on mongoose commands

--- backend/src/models/mongoose.js
+++ backend/src/models/mongoose.js
@@ -11,21 +11,27 @@ let options = {
     keepAlive: 100,
     connectTimeoutMS: 30000,
     socketTimeoutMS: 30000,
-    reconnectTries: 3
+    reconnectTries: 3,
+    useNewUrlParser: true,
+    bufferCommands: false,
+    bufferMaxEntries: 0
 };
 
-mongoose.connect(DB.URI, options);
+mongoose.connect(
+    DB.URI,
+    options
+);
 
-mongoose.connection.on("error", function (error) {
+mongoose.connection.on("error", function(error) {
     Logger.error(error, "Error connecting to MongoDB");
     throw error;
 });
 
-mongoose.connection.on("connected", function () {
+mongoose.connection.on("connected", function() {
     Logger.info("Connection established to MongoDB");
 });
 
-mongoose.connection.on("reconnected", function () {
+mongoose.connection.on("reconnected", function() {
     Logger.info("Reconnected to MongoDB");
 });
 
