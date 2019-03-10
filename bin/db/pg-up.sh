d7035f378a3f25c9d3c8ccde8a601426da475af9
Quek Ruo Ling <>
2019-03-10 20:28:56 +0800

    [Scripts] Read postgres db credentials from env var

--- bin/db/pg-up.sh
+++ bin/db/pg-up.sh
@@ -1,2 +1,6 @@
 docker pull postgres
-docker run --name postgres --restart=always -d -p 5432:5432 --network network-rollplays postgres
+docker run --name postgres \
+-e POSTGRES_USER="$POSTGRES_USER" \
+-e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+-e POSTGRES_DB="$POSTGRES_DB" \
+-d -p 5432:5432 postgres
