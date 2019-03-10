8dcfb766ccf8cbbd64fe0d8c6b058847e48bbd5d
Quek Ruo Ling <>
2019-03-10 21:27:27 +0800

    [Scripts] Add postgres to docker network

--- bin/db/pg-up.sh
+++ bin/db/pg-up.sh
@@ -3,4 +3,4 @@ docker run --name postgres \
 -e POSTGRES_USER="$POSTGRES_USER" \
 -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
 -e POSTGRES_DB="$POSTGRES_DB" \
--d -p 5432:5432 postgres
+-d -p 5432:5432 --network network-rollplays postgres
