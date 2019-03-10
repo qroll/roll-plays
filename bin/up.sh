950b56bb8c9b596ae381fc2885bb76be84e01b69
Quek Ruo Ling <>
2019-03-10 19:46:00 +0800

    [Scripts] Set up postgres database

--- bin/up.sh
+++ bin/up.sh
@@ -11,12 +11,15 @@ echo "Setting up shared configuration"
 echo "==============================="
 sh "$SCRIPT_DIR/shared/shared-up.sh"
 
-echo "\n==================="
+echo
+echo "==================="
 echo "Setting up database"
 echo "==================="
 sh "$SCRIPT_DIR/db/mongo-up.sh"
+sh "$SCRIPT_DIR/db/pg-up.sh"
 
-echo "\n=============="
+echo
+echo "=============="
 echo "Setting up app"
 echo "=============="
 sh "$SCRIPT_DIR/app/app-up.sh"
